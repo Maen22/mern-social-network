237184d6377ac99f956efec4436c7f23961e130f
Maen22 <m3n.ibregeth@gmail.com>
2021-02-06 19:17:35 +0200

    more imports refactoring

--- client/src/MainRouter.js
+++ client/src/MainRouter.js
@@ -1,11 +1,13 @@
 import { Route, Switch } from "react-router-dom";
 import Home from "./core/Home";
+import Users from "./user/Users";
 
 const MainRouter = () => {
   return (
     <div>
       <Switch>
         <Route to="/" component={Home} exact />
+        <Route path="/users" component={Users} />
       </Switch>
     </div>
   );
--- client/src/core/Home.js
+++ client/src/core/Home.js
@@ -1,9 +1,7 @@
 import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Typography from "@material-ui/core/Typography";
+import { Card, CardContent, CardMedia, Typography } from "@material-ui/core";
 import unicornbikeImg from "../assests/images/unicornbike.jpg";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   card: {
@@ -29,23 +27,26 @@ const Home = () => {
   const classes = useStyles();
 
   return (
-    <Card className={classes.card}>
-      <Typography variant="h6" className={classes.title}>
-        Home Page
-      </Typography>
-
-      <CardMedia
-        image={unicornbikeImg}
-        className={classes.media}
-        title="Unicon Bicycle"
-      />
-
-      <CardContent>
-        <Typography variant="body2" component="p">
-          Welcome to the MERN home page.
+    <>
+      <Link to="/users">Users</Link>
+      <Card className={classes.card}>
+        <Typography variant="h6" className={classes.title}>
+          Home Page
         </Typography>
-      </CardContent>
-    </Card>
+
+        <CardMedia
+          image={unicornbikeImg}
+          className={classes.media}
+          title="Unicon Bicycle"
+        />
+
+        <CardContent>
+          <Typography variant="body2" component="p">
+            Welcome to the MERN home page.
+          </Typography>
+        </CardContent>
+      </Card>
+    </>
   );
 };
 
